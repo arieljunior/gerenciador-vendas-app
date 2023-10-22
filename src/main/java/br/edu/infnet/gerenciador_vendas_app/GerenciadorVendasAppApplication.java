4eba04a4f6ea5ca50eb4a7cd3147d7fcbe9b2e7b
Ariel Junior <act117@einstein.br>
2023-10-22 17:16:21 -0300

    primeiro commit

---
+++ src/main/java/br/edu/infnet/gerenciador_vendas_app/GerenciadorVendasAppApplication.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.gerenciador_vendas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorVendasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorVendasAppApplication.class, args);
+		System.out.print("Hello world!");
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/gerenciador_vendas_app/GerenciadorVendasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gerenciador_vendas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorVendasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
